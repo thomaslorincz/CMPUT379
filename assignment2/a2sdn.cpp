8e6864117a80e868a66771363307d2bca20d7acb
Thomas Lorincz <tlorincz@ualberta.ca>
2018-10-27 21:40:55 -0600

    Polling keyboard and FIFOs in controller loop.

--- assignment2/a2sdn.cpp
+++ assignment2/a2sdn.cpp
@@ -2,36 +2,38 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <poll.h>
+#include <string.h>  // memset(3)
 #include <sys/resource.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <fstream>
+#include <algorithm>  // std::find_if
+#include <fstream>    // std::fstream::in
 #include <iostream>
 #include <sstream>
-#include <string>
-#include <tuple>
-#include <vector>
+#include <string>  // std::string
+#include <tuple>   // std::tuple
+#include <vector>  // std::vector
 
 using namespace std;
 
-static const int CONTROLLER_ID = 0;
-static const int MAX_NSW = 7;
-static const int MAXIP = 1000;
-static const int MINPRI = 4;
-static const int MAX_BUFFER = 1024;
+#define CONTROLLER_ID 0
+#define MAX_NSW 7
+#define MAXIP 1000
+#define MINPRI 4
+#define MAX_BUFFER 1024
 
 typedef enum { OPEN, ACK, QUERY, ADD, RELAY } PACKET_TYPE;
 
-struct packet {
+typedef struct {
   PACKET_TYPE type;
   int id;
   int ip_low;
   int ip_high;
-};
+} packet;
 
-struct flow_rule {
+typedef struct {
   int srcIP_lo;
   int srcIP_hi;
   int destIP_lo;
@@ -40,14 +42,33 @@ struct flow_rule {
   int actionVal;
   int pri;  // 0, 1, 2, 3, 4 (highest - lowest)
   int pktCount;
-};
+} flow_rule;
 
 typedef enum { SEND, RECEIVE } MODE;
 
-struct connection {
+typedef struct {
   MODE mode;
   string name;
-};
+} connection;
+
+// Trim from start (in place)
+static inline void ltrim(string &s) {
+  s.erase(s.begin(),
+          find_if(s.begin(), s.end(), [](int ch) { return !isspace(ch); }));
+}
+
+// Trim from end (in place)
+static inline void rtrim(string &s) {
+  s.erase(
+      find_if(s.rbegin(), s.rend(), [](int ch) { return !isspace(ch); }).base(),
+      s.end());
+}
+
+// Trim from both ends (in place)
+static inline void trim(string &s) {
+  ltrim(s);
+  rtrim(s);
+}
 
 string make_fifo_name(int sender_id, int receiver_id) {
   return "fifo-" + to_string(sender_id) + "-" + to_string(receiver_id);
@@ -143,7 +164,7 @@ void switch_loop(int id, tuple<int, int> ip_range, vector<flow_rule> flow_table,
 
   // Send an OPEN packet to the controller
   packet open_packet = {OPEN, id, get<0>(ip_range), get<1>(ip_range)};
-  write(pfds[0].fd, &open_packet, sizeof(struct packet));
+  write(pfds[0].fd, &open_packet, sizeof(packet));
 
   // TODO: Read file properly. This is just a test.
   string str;
@@ -152,11 +173,14 @@ void switch_loop(int id, tuple<int, int> ip_range, vector<flow_rule> flow_table,
   }
 }
 
+void controller_list() {}
+
 void controller_loop(int num_switches) {
-  errno = 0;
+  errno = 0;  // Reset errno in case it has been set
 
   vector<connection> connections;
 
+  // Generate receiver connections and FIFO names
   for (int i = 1; i <= num_switches; i++) {
     connections.push_back({RECEIVE, make_fifo_name(i, CONTROLLER_ID)});
   }
@@ -186,8 +210,64 @@ void controller_loop(int num_switches) {
 
     j++;
   }
+
+  while (true) {
+    memset(buffer, 0, sizeof(buffer));  // Clear buffer
+
+    /*
+     * 1. Poll the keyboard for a user command. The user can issue one of the
+     * following commands. list: The program writes all entries in the flow
+     * table, and for each transmitted or received packet type, the program
+     * writes an aggregate count of handled packets of this type. exit: The
+     * program writes the above information and exits.
+     */
+    poll(pfds, connections.size() + 1, 0);  // Poll from all file descriptors
+
+    if (pfds[0].revents & POLLIN) {
+      int r = read(pfds[0].fd, buffer, MAX_BUFFER);
+      if (!r) printf("Warning: stdin closed.\n");
+
+      string cmd = string(buffer);
+      trim(cmd);  // Trim whitespace
+
+      if (cmd == "list") {
+        controller_list();
+      } else if (cmd == "exit") {
+        controller_list();
+        exit(0);
+      } else {
+        printf("Error: Unrecognized command. Please use list or exit.\n");
+      }
+    }
+
+    /*
+     * 2. Poll the incoming FIFOs from the attached switches. The controller
+     * handles each incoming packet, as described in the Packet Types section.
+     */
+    int k = 1;  // FIFOs start at index 1 in pfds
+    for (auto &this_connection : connections) {
+      if (pfds[k].revents & POLLIN) {
+        memset(buffer, 0, sizeof(buffer));  // Clear buffer
+
+        int r = read(pfds[k].fd, buffer, MAX_BUFFER);
+        if (!r) {
+          printf("Warning: Connection %s closed.\n",
+                 this_connection.name.c_str());
+        }
+        string cmd = string(buffer);
+
+        printf("Received command: %s\n", cmd);
+      }
+
+      k++;
+    }
+  }
 }
 
+/**
+ * Main function. Processes command line arguments into inputs for either the
+ * controller loop or the switch loop.
+ */
 int main(int argc, char **argv) {
   // Set a 10 minute CPU time limit
   rlimit time_limit{.rlim_cur = 600, .rlim_max = 600};
