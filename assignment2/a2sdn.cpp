38113f00e0ee68383b3ec8537dd37a6c519f4839
Thomas Lorincz <tlorincz@ualberta.ca>
2018-10-30 11:39:39 -0600

    Modularized code. Changed naming according to Google style guide.

--- assignment2/a2sdn.cpp
+++ assignment2/a2sdn.cpp
@@ -1,119 +1,19 @@
-#include <errno.h>
 #include <fcntl.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <poll.h>
-#include <string.h>  // memset, strlen
 #include <sys/resource.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <algorithm>  // std::find_if
-#include <fstream>    // std::fstream::in
-#include <iostream>
-#include <map>  // std::map
+#include <fstream>
 #include <sstream>
-#include <string>  // std::string, std::to_string
-#include <tuple>   // std::tuple
-#include <vector>  // std::vector
+#include <string>
+#include <tuple>
+#include "controller.h"
+#include "switch.h"
+
+#define MAX_NSW 7
+#define MAXIP 1000
 
 using namespace std;
 
-#define CONTROLLER_ID 0
-#define MAX_NSW 7
-#define MAXIP 1000
-#define MINPRI 4
-#define MAX_BUFFER 1024
-
-typedef enum { OPEN, ACK, QUERY, ADD, RELAY } PACKET_TYPE;
-
-typedef struct {
-  PACKET_TYPE type;
-  string message;
-} packet;
-
-typedef struct {
-  int srcIP_lo;
-  int srcIP_hi;
-  int destIP_lo;
-  int destIP_hi;
-  string actionType;  // FORWARD, DROP
-  int actionVal;
-  int pri;  // 0, 1, 2, 3, 4 (highest - lowest)
-  int pktCount;
-} flow_rule;
-
-typedef struct {
-  int fd;
-  string fifo;
-} connection;
-
-typedef struct {
-  int id;
-  int port1Id;
-  int port2Id;
-  int ipLow;
-  int ipHigh;
-} switch_info;
-
-string packet_to_string(packet &p) {
-  return to_string(p.type) + ":" + p.message;
-}
-
-packet parse_packet_string(string &s) {
-  string packet_type_token = s.substr(0, s.find(":"));
-  PACKET_TYPE type =
-      (PACKET_TYPE)strtol(packet_type_token.c_str(), (char **)NULL, 10);
-
-  string packet_message_token = s.substr(s.find(":") + 1);
-
-  return {type, packet_message_token};
-}
-
-switch_info parse_open_message(string &m) {
-  vector<int> vect;
-  stringstream ss(m);
-
-  // Split packet string into ints (comma delimited)
-  int i = 0;
-  while (ss >> i) {
-    vect.push_back(i);
-    if (ss.peek() == ',') ss.ignore();
-  }
-
-  if (vect.size() < 5) {
-    printf("Error: Invalid OPEN packet.\n");
-  }
-
-  return {vect[0], vect[1], vect[2], vect[3], vect[4]};
-}
-
-// Trim from start (in place)
-static inline void ltrim(string &s) {
-  s.erase(s.begin(),
-          find_if(s.begin(), s.end(), [](int ch) { return !isspace(ch); }));
-}
-
-// Trim from end (in place)
-static inline void rtrim(string &s) {
-  s.erase(
-      find_if(s.rbegin(), s.rend(), [](int ch) { return !isspace(ch); }).base(),
-      s.end());
-}
-
-// Trim from both ends (in place)
-static inline void trim(string &s) {
-  ltrim(s);
-  rtrim(s);
-}
-
-string make_fifo_name(int sender_id, int receiver_id) {
-  return "fifo-" + to_string(sender_id) + "-" + to_string(receiver_id);
-}
-
 // TODO: Perhaps error handling
-int parse_switch_id(const string &input) {
+int ParseSwitchId(const string &input) {
   if (input == "null") {
     return -1;
   } else {
@@ -122,7 +22,7 @@ int parse_switch_id(const string &input) {
   }
 }
 
-tuple<int, int> process_ip_range(const string &input) {
+tuple<int, int> ParseIpRange(const string &input) {
   int ip_low;
   int ip_high;
 
@@ -144,225 +44,6 @@ tuple<int, int> process_ip_range(const string &input) {
   }
 
   return make_tuple(ip_low, ip_high);
-}
-
-void switch_list() { printf("List:\n"); }
-
-void switch_loop(int id, int port_1_id, int port_2_id, tuple<int, int> ip_range,
-                 ifstream &in) {
-  vector<flow_rule> flow_table;
-  flow_rule initial_rule = {
-      0, MAXIP, get<0>(ip_range), get<1>(ip_range), "FORWARD", 3, MINPRI, 0};
-  flow_table.push_back(initial_rule);
-
-  vector<int> activeIds;
-  activeIds.push_back(id);
-  if (port_1_id != -1) activeIds.push_back(port_1_id);
-  if (port_2_id != -1) activeIds.push_back(port_2_id);
-
-  vector<connection> send_connections;
-
-  char buffer[MAX_BUFFER];
-  struct pollfd pfds[activeIds.size() + 1];
-  pfds[0].fd = STDIN_FILENO;
-  pfds[0].events = POLLIN;
-  pfds[0].revents = 0;
-
-  // TODO:
-
-  // Send an OPEN packet to the controller
-  string open_message = to_string(OPEN) + ":" + to_string(id) + "," +
-                        to_string(port_1_id) + "," + to_string(port_2_id) +
-                        "," + to_string(get<0>(ip_range)) + "," +
-                        to_string(get<1>(ip_range));
-  write(pfds[1].fd, open_message.c_str(), strlen(open_message.c_str()));
-  if (errno) perror("Error: Failed to write.\n");
-  errno = 0;
-
-  while (true) {
-    poll(pfds, activeIds.size() + 1, 0);
-    if (errno) perror("Error: poll() failure.\n");
-    errno = 0;
-
-    if (pfds[0].revents & POLLIN) {
-      ssize_t r = read(pfds[0].fd, buffer, MAX_BUFFER);
-      if (!r) printf("Warning: stdin closed.\n");
-
-      string cmd = string(buffer);
-      trim(cmd);  // Trim whitespace
-
-      if (cmd == "list") {
-        switch_list();
-      } else if (cmd == "exit") {
-        switch_list();
-        exit(0);
-      } else {
-        printf("Error: Unrecognized command. Please use list or exit.\n");
-      }
-    }
-
-    /*
-     * 3. Poll the incoming FIFOs from the controller and the attached switches.
-     * The switch handles each incoming packet, as described in the Packet Types
-     * section.
-     */
-    for (int i = 1; i <= (int)activeIds.size(); i++) {
-      if (pfds[i].revents & POLLIN) {
-        ssize_t r = read(pfds[i].fd, buffer, MAX_BUFFER);
-        if (!r) {
-          printf("Warning: Connection closed.\n");
-        }
-        string packet_string = string(buffer);
-        packet received_packet = parse_packet_string(packet_string);
-
-        printf("Received packet: %s\n", buffer);
-
-        if (received_packet.type == ACK) {
-          // Ignore
-        } else if (received_packet.type == ADD) {
-          // Ignore for now
-        } else if (received_packet.type == RELAY) {
-          // Ignore for now
-        } else {
-          printf("Received %s packet. Ignored.\n",
-                 to_string(received_packet.type).c_str());
-        }
-      }
-    }
-
-    memset(buffer, 0, sizeof(buffer));  // Clear buffer
-  }
-}
-
-void controller_list(vector<switch_info> switchInfoTable, int open, int query,
-                     int ack, int add) {
-  printf("Switch information:\n");
-  for (auto &this_info : switchInfoTable) {
-    printf("[sw%i]: port1= %i, port2= %i, port3= %i-%i\n", this_info.id,
-           this_info.port1Id, this_info.port2Id, this_info.ipLow,
-           this_info.ipHigh);
-  }
-  printf("Packet stats:\n");
-  printf("\tReceived:    OPEN:%i, QUERY:%i\n", open, query);
-  printf("\tTransmitted: ACK:%i, ADD:%i\n", ack, add);
-}
-
-void controller_loop(int num_switches) {
-  vector<switch_info> switchInfoTable;
-  int openCount = 0;
-  int queryCount = 0;
-  int ackCount = 0;
-  int addCount = 0;
-
-  map<int, int> id_to_fd;  // Mapping switch IDs to FDs
-
-  struct pollfd pfds[num_switches + 1];
-  pfds[0].fd = STDIN_FILENO;
-  pfds[0].events = POLLIN;
-  pfds[0].revents = 0;
-  char buffer[MAX_BUFFER];
-
-  for (int i = 1; i <= num_switches; i++) {
-    string fifo_name = make_fifo_name(i, CONTROLLER_ID);
-    mkfifo(fifo_name.c_str(),
-           S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    if (errno) perror("Error: Could not create a FIFO connection.\n");
-    errno = 0;
-
-    // Returns lowest unused file descriptor on success
-    int fd = open(fifo_name.c_str(), O_RDONLY | O_NONBLOCK);
-    if (errno) perror("Error: Could not open FIFO.\n");
-    errno = 0;
-
-    printf("Created %s fd = %i\n", fifo_name.c_str(), fd);
-
-    pfds[i].fd = fd;
-    pfds[i].events = POLLIN;
-    pfds[i].revents = 0;
-  }
-
-  while (true) {
-    /*
-     * 1. Poll the keyboard for a user command. The user can issue one of the
-     * following commands. list: The program writes all entries in the flow
-     * table, and for each transmitted or received packet type, the program
-     * writes an aggregate count of handled packets of this type. exit: The
-     * program writes the above information and exits.
-     */
-    poll(pfds, num_switches + 1, 0);  // Poll from all file descriptors
-    if (errno) perror("Error: poll() failure.\n");
-    errno = 0;
-
-    if (pfds[0].revents & POLLIN) {
-      ssize_t r = read(pfds[0].fd, buffer, MAX_BUFFER);
-      if (!r) printf("Warning: stdin closed.\n");  // TODO: errno?
-
-      string cmd = string(buffer);
-      trim(cmd);  // Trim whitespace
-
-      if (cmd == "list") {
-        controller_list(switchInfoTable, openCount, queryCount, ackCount,
-                        addCount);
-      } else if (cmd == "exit") {
-        controller_list(switchInfoTable, openCount, queryCount, ackCount,
-                        addCount);
-        exit(0);
-      } else {
-        printf("Error: Unrecognized command. Please use list or exit.\n");
-      }
-    }
-
-    /*
-     * 2. Poll the incoming FIFOs from the attached switches. The controller
-     * handles each incoming packet, as described in the Packet Types section.
-     */
-    for (int i = 1; i < num_switches; i++) {
-      if (pfds[i].revents & POLLIN) {
-        ssize_t r = read(pfds[i].fd, buffer, MAX_BUFFER);
-        if (!r) {
-          string closed_fifo = make_fifo_name(i, CONTROLLER_ID);
-          printf("Warning: Connection %s closed.\n", closed_fifo.c_str());
-        }
-        string packet_string = string(buffer);
-        packet received_packet = parse_packet_string(packet_string);
-
-        printf("Received packet: %s\n", buffer);
-
-        if (received_packet.type == OPEN) {
-          openCount++;
-
-          switch_info new_info = parse_open_message(received_packet.message);
-          switchInfoTable.push_back(new_info);
-
-          // Returns lowest unused file descriptor on success
-          string fifo_name = make_fifo_name(CONTROLLER_ID, i);
-          int fd = open(fifo_name.c_str(), O_WRONLY | O_NONBLOCK);
-          if (errno) perror("Error: Could not open FIFO.\n");
-          errno = 0;
-          id_to_fd.insert({i, fd});
-
-          string ack_message = to_string(ACK) + ":";
-
-          write(fd, ack_message.c_str(), strlen(ack_message.c_str()));
-          if (errno) perror("Error: Could not write.\n");
-          errno = 0;
-        } else if (received_packet.type == QUERY) {
-          queryCount++;
-          // TODO: Reply with an ADD packet (message is the flow table rule)
-          string add_message = to_string(ADD) + ":";
-
-          write(id_to_fd[i], add_message.c_str(), strlen(add_message.c_str()));
-          if (errno) perror("Error: Could not write.\n");
-          errno = 0;
-        } else {
-          printf("Received %s packet. Ignored.\n",
-                 to_string(received_packet.type).c_str());
-        }
-      }
-    }
-
-    memset(buffer, 0, sizeof(buffer));  // Clear buffer
-  }
 }
 
 /**
@@ -392,15 +73,15 @@ int main(int argc, char **argv) {
       return 1;
     }
 
-    controller_loop(num_switches);
+    ControllerLoop(num_switches);
   } else if (mode.find("sw") != std::string::npos) {
     if (argc != 6) {
       printf("Error: Invalid number of arguments. Expected 6.\n");
       return 1;
     }
 
-    int switchId = parse_switch_id(argv[1]);
-    if (switchId == -1) {
+    int switch_id = ParseSwitchId(argv[1]);
+    if (switch_id == -1) {
       printf("Error: Switch ID is invalid.\n");
       return 1;
     }
@@ -412,17 +93,17 @@ int main(int argc, char **argv) {
       return 1;
     }
 
-    int switchId1 = parse_switch_id(argv[3]);
-    int switchId2 = parse_switch_id(argv[4]);
+    int switch_id_1 = ParseSwitchId(argv[3]);
+    int switch_id_2 = ParseSwitchId(argv[4]);
 
-    tuple<int, int> ipRange = process_ip_range(argv[5]);
+    tuple<int, int> ip_range = ParseIpRange(argv[5]);
 
-    if (get<0>(ipRange) == -1 && get<1>(ipRange) == -1) {
+    if (get<0>(ip_range) == -1 && get<1>(ip_range) == -1) {
       printf("Error: Malformed IP range.\n");
       return 1;
     }
 
-    switch_loop(switchId, switchId1, switchId2, ipRange, in);
+    SwitchLoop(switch_id, switch_id_1, switch_id_2, ip_range, in);
   }
 
   return 0;
