6c4b85463048aa1c7876108e1b3ac58b86b90748
Thomas Lorincz <tlorincz@ualberta.ca>
2018-10-29 21:16:30 -0600

    Controller loop is set up. Switch loop setup in progress.

--- assignment2/a2sdn.cpp
+++ assignment2/a2sdn.cpp
@@ -3,7 +3,7 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <poll.h>
-#include <string.h>  // memset
+#include <string.h>  // memset, strlen
 #include <sys/resource.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
@@ -12,6 +12,7 @@
 #include <algorithm>  // std::find_if
 #include <fstream>    // std::fstream::in
 #include <iostream>
+#include <map>  // std::map
 #include <sstream>
 #include <string>  // std::string, std::to_string
 #include <tuple>   // std::tuple
@@ -43,11 +44,9 @@ typedef struct {
   int pktCount;
 } flow_rule;
 
-typedef enum { SEND, RECEIVE } MODE;
-
 typedef struct {
-  MODE mode;
-  string name;
+  int fd;
+  string fifo;
 } connection;
 
 string packet_to_string(packet &p) {
@@ -75,6 +74,10 @@ vector<int> parse_open_message(string &m) {
     if (ss.peek() == ',') ss.ignore();
   }
 
+  if (vect.size() < 5) {
+    printf("Error: Invalid OPEN packet.\n");
+  }
+
   return vect;
 }
 
@@ -99,26 +102,6 @@ static inline void trim(string &s) {
 
 string make_fifo_name(int sender_id, int receiver_id) {
   return "fifo-" + to_string(sender_id) + "-" + to_string(receiver_id);
-}
-
-vector<connection> make_connections(int switch_id, int port_1_switch_id,
-                                    int port_2_switch_id) {
-  vector<connection> output;
-
-  output.push_back({SEND, make_fifo_name(switch_id, CONTROLLER_ID)});
-  output.push_back({RECEIVE, make_fifo_name(CONTROLLER_ID, switch_id)});
-
-  if (port_1_switch_id != -1) {
-    output.push_back({SEND, make_fifo_name(switch_id, port_1_switch_id)});
-    output.push_back({RECEIVE, make_fifo_name(port_1_switch_id, switch_id)});
-  }
-
-  if (port_2_switch_id != -1) {
-    output.push_back({SEND, make_fifo_name(switch_id, port_2_switch_id)});
-    output.push_back({RECEIVE, make_fifo_name(port_2_switch_id, switch_id)});
-  }
-
-  return output;
 }
 
 // TODO: Perhaps error handling
@@ -155,93 +138,127 @@ tuple<int, int> process_ip_range(const string &input) {
   return make_tuple(ip_low, ip_high);
 }
 
+void switch_list() { printf("List:\n"); }
+
 void switch_loop(int id, int port_1_id, int port_2_id, tuple<int, int> ip_range,
-                 vector<flow_rule> flow_table, vector<connection> connections,
-                 ifstream &in) {
+                 vector<flow_rule> flow_table, ifstream &in) {
+  vector<int> activeIds;
+  activeIds.push_back(id);
+  if (port_1_id != -1) activeIds.push_back(port_1_id);
+  if (port_2_id != -1) activeIds.push_back(port_2_id);
+
+  vector<connection> receive_connections;
+  vector<connection> send_connections;
+
   char buffer[MAX_BUFFER];
-  struct pollfd pfds[2 * connections.size() + 1];  // Both way connections
+  struct pollfd pfds[activeIds.size() + 1];
   pfds[0].fd = STDIN_FILENO;
+  pfds[0].events = POLLIN;
+  pfds[0].revents = 0;
 
-  // Create and open FIFOs for all connections
-  int i = 1;
-  for (auto &this_connection : connections) {
-    errno = 0;
-    int status =
-        mkfifo(this_connection.name.c_str(),
-               S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-
-    if (errno != 0 && errno != EEXIST) {
-      printf("FIFO: %s\n", this_connection.name.c_str());
-      perror("Error: Could not create a FIFO connection.\n");
-    }
-    errno = 0;
-
-    int rw_flag = (this_connection.mode == SEND) ? O_WRONLY : O_RDONLY;
-
-    // Returns lowest unused file descriptor on success
-    int fd = open(this_connection.name.c_str(), rw_flag | O_NONBLOCK);
-    if (errno) {
-      printf("FIFO: %s\n", this_connection.name.c_str());
-      perror("Error: Could not open FIFO.\n");
-    }
-    errno = 0;
-
-    pfds[i].fd = fd;
-
-    i++;
-  }
+  // TODO:
 
   // Send an OPEN packet to the controller
-  string open_message =
-      to_string(id) + "," + to_string(port_1_id) + "," + to_string(port_2_id) +
-      "," + to_string(get<0>(ip_range)) + "," + to_string(get<1>(ip_range));
-  packet open_packet = {OPEN, open_message};
-  write(pfds[1].fd, &open_packet, sizeof(packet));
+  string open_message = to_string(OPEN) + ":" + to_string(id) + "," +
+                        to_string(port_1_id) + "," + to_string(port_2_id) +
+                        "," + to_string(get<0>(ip_range)) + "," +
+                        to_string(get<1>(ip_range));
+  write(pfds[1].fd, open_message.c_str(), strlen(open_message.c_str()));
+  if (errno) perror("Error: Failed to write.\n");
+  errno = 0;
 
-  // while (true) {}
+  while (true) {
+    poll(pfds, activeIds.size() + 1, 0);
+    if (errno) perror("Error: poll() failure.\n");
+    errno = 0;
+
+    if (pfds[0].revents & POLLIN) {
+      ssize_t r = read(pfds[0].fd, buffer, MAX_BUFFER);
+      if (!r) printf("Warning: stdin closed.\n");
+
+      string cmd = string(buffer);
+      trim(cmd);  // Trim whitespace
+
+      if (cmd == "list") {
+        switch_list();
+      } else if (cmd == "exit") {
+        switch_list();
+        exit(0);
+      } else {
+        printf("Error: Unrecognized command. Please use list or exit.\n");
+      }
+    }
+
+    /*
+     * 3. Poll the incoming FIFOs from the controller and the attached switches.
+     * The switch handles each incoming packet, as described in the Packet Types
+     * section.
+     */
+    for (int i = 1; i <= (int)activeIds.size(); i++) {
+      if (pfds[i].revents & POLLIN) {
+        ssize_t r = read(pfds[i].fd, buffer, MAX_BUFFER);
+        if (!r) {
+          printf("Warning: Connection %s closed.\n",
+                 receive_connections[i].fifo.c_str());
+        }
+        string packet_string = string(buffer);
+        packet received_packet = parse_packet_string(packet_string);
+
+        printf("Received packet: %s\n", buffer);
+
+        if (received_packet.type == ACK) {
+          // Ignore
+        } else if (received_packet.type == ADD) {
+          // Ignore for now
+        } else if (received_packet.type == RELAY) {
+          // Ignore for now
+        } else {
+          printf("Received %s packet. Ignored.\n",
+                 to_string(received_packet.type).c_str());
+        }
+      }
+    }
+
+    memset(buffer, 0, sizeof(buffer));  // Clear buffer
+  }
 }
 
-void controller_list() {}
+void controller_list() { printf("List:\n"); }
 
 void controller_loop(int num_switches) {
-  errno = 0;  // Reset errno in case it has been set
-
-  vector<connection> connections;
-
   // Generate receiver connections and FIFO names
-  for (int i = 1; i <= num_switches; i++) {
-    connections.push_back({RECEIVE, make_fifo_name(i, CONTROLLER_ID)});
-  }
+  vector<connection> receive_connections;
+  vector<connection> send_connections;
 
-  struct pollfd pfds[2 * connections.size() + 1];
+  struct pollfd pfds[num_switches + 1];
   pfds[0].fd = STDIN_FILENO;
+  pfds[0].events = POLLIN;
+  pfds[0].revents = 0;
   char buffer[MAX_BUFFER];
 
-  int j = 0;
-  for (auto &this_connection : connections) {
-    int status =
-        mkfifo(this_connection.name.c_str(),
-               S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-    if (errno || status == -1) {
-      printf("FIFO: %s\n", this_connection.name.c_str());
-      perror("Error: Could not create a FIFO connection.\n");
-    }
+  for (int i = 1; i <= num_switches; i++) {
+    string fifo_name = make_fifo_name(i, CONTROLLER_ID);
+    mkfifo(fifo_name.c_str(),
+           S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
+    if (errno) perror("Error: Could not create a FIFO connection.\n");
+    errno = 0;
 
     // Returns lowest unused file descriptor on success
-    int fd = open(this_connection.name.c_str(), O_RDONLY | O_NONBLOCK);
-    if (errno || fd == -1) {
-      printf("FIFO: %s\n", this_connection.name.c_str());
-      perror("Error: Could not open FIFO.\n");
-    }
+    int fd = open(fifo_name.c_str(), O_RDONLY | O_NONBLOCK);
+    if (errno) perror("Error: Could not open FIFO.\n");
+    errno = 0;
 
-    pfds[j].fd = fd;
+    printf("Created %s fd = %i\n", fifo_name.c_str(), fd);
 
-    j++;
+    pfds[i].fd = fd;
+    pfds[i].events = POLLIN;
+    pfds[i].revents = 0;
+
+    send_connections.push_back({-1, ""});  // Placeholders
+    receive_connections.push_back({fd, fifo_name});
   }
 
   while (true) {
-    memset(buffer, 0, sizeof(buffer));  // Clear buffer
-
     /*
      * 1. Poll the keyboard for a user command. The user can issue one of the
      * following commands. list: The program writes all entries in the flow
@@ -249,11 +266,13 @@ void controller_loop(int num_switches) {
      * writes an aggregate count of handled packets of this type. exit: The
      * program writes the above information and exits.
      */
-    poll(pfds, connections.size() + 1, 0);  // Poll from all file descriptors
+    poll(pfds, num_switches + 1, 0);  // Poll from all file descriptors
+    if (errno) perror("Error: poll() failure.\n");
+    errno = 0;
 
     if (pfds[0].revents & POLLIN) {
-      int r = read(pfds[0].fd, buffer, MAX_BUFFER);
-      if (!r) printf("Warning: stdin closed.\n");
+      ssize_t r = read(pfds[0].fd, buffer, MAX_BUFFER);
+      if (!r) printf("Warning: stdin closed.\n");  // TODO: errno?
 
       string cmd = string(buffer);
       trim(cmd);  // Trim whitespace
@@ -272,23 +291,51 @@ void controller_loop(int num_switches) {
      * 2. Poll the incoming FIFOs from the attached switches. The controller
      * handles each incoming packet, as described in the Packet Types section.
      */
-    int k = 1;  // FIFOs start at index 1 in pfds
-    for (auto &this_connection : connections) {
-      if (pfds[k].revents & POLLIN) {
-        memset(buffer, 0, sizeof(buffer));  // Clear buffer
-
-        int r = read(pfds[k].fd, buffer, MAX_BUFFER);
+    for (int i = 1; i < num_switches; i++) {
+      if (pfds[i].revents & POLLIN) {
+        ssize_t r = read(pfds[i].fd, buffer, MAX_BUFFER);
         if (!r) {
           printf("Warning: Connection %s closed.\n",
-                 this_connection.name.c_str());
+                 receive_connections[i].fifo.c_str());
         }
-        string cmd = string(buffer);
+        string packet_string = string(buffer);
+        packet received_packet = parse_packet_string(packet_string);
 
-        printf("Received command: %s\n", cmd);
+        printf("Received packet: %s\n", buffer);
+
+        if (received_packet.type == OPEN) {
+          // TODO: Make flow table rule
+          vector<int> package_data =
+              parse_open_message(received_packet.message);
+
+          // Returns lowest unused file descriptor on success
+          string fifo_name = make_fifo_name(CONTROLLER_ID, i);
+          int fd = open(fifo_name.c_str(), O_WRONLY | O_NONBLOCK);
+          if (errno) perror("Error: Could not open FIFO.\n");
+          errno = 0;
+          send_connections[i] = {fd, fifo_name};
+
+          string ack_message = to_string(ACK) + ":";
+
+          write(fd, ack_message.c_str(), strlen(ack_message.c_str()));
+          if (errno) perror("Error: Could not write.\n");
+          errno = 0;
+        } else if (received_packet.type == QUERY) {
+          // TODO: Reply with an ADD packet (message is the flow table rule)
+          string add_message = to_string(ADD) + ":";
+
+          write(send_connections[i].fd, add_message.c_str(),
+                strlen(add_message.c_str()));
+          if (errno) perror("Error: Could not write.\n");
+          errno = 0;
+        } else {
+          printf("Received %s packet. Ignored.\n",
+                 to_string(received_packet.type).c_str());
+        }
       }
-
-      k++;
     }
+
+    memset(buffer, 0, sizeof(buffer));  // Clear buffer
   }
 }
 
@@ -318,8 +365,6 @@ int main(int argc, char **argv) {
       printf("Error: Invalid number of switches. Must be 1-7.\n");
       return 1;
     }
-
-    printf("%i\n", num_switches);
 
     controller_loop(num_switches);
   } else if (mode.find("sw") != std::string::npos) {
@@ -356,11 +401,7 @@ int main(int argc, char **argv) {
         0, MAXIP, get<0>(ip_range), get<1>(ip_range), "FORWARD", 3, MINPRI, 0};
     flow_table.push_back(initial_rule);
 
-    vector<connection> connections =
-        make_connections(switch_id, switchId1, switchId2);
-
-    switch_loop(switch_id, switchId1, switchId2, ip_range, flow_table,
-                connections, in);
+    switch_loop(switch_id, switchId1, switchId2, ip_range, flow_table, in);
   }
 
   return 0;
